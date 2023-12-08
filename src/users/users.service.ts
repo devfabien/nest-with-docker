83d9e3f448e72e360faf6e2b9ab92323d58c1797
devfabien <ish001fabi@gmail.com>
2023-12-08 15:52:08 +0200

    feat: creating service functions

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './schemas/user.entity';
+import { DeleteResult, Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
+
+  async findOne(id: number): Promise<User | null> {
+    return await this.userRepository.findOneBy({ id });
+  }
+
+  async create(user: User): Promise<User> {
+    return await this.userRepository.save(user);
+  }
+
+  async remove(id: string): Promise<DeleteResult> {
+    return await this.userRepository.delete(id);
+  }
+}
