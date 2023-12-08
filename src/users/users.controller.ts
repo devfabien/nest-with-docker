4c3851d8323bf9cfd6f0b0e878646e988f259201
devfabien <ish001fabi@gmail.com>
2023-12-08 15:51:36 +0200

    feat: creating the controller requests

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './schemas/user.entity';
+import { DeleteResult } from 'typeorm';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  async getAllUsers(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  async getUser(@Param('id') id: number): Promise<User> {
+    return this.usersService.findOne(id);
+  }
+
+  @Post()
+  async createUser(@Body() user: User): Promise<User> {
+    return this.usersService.create(user);
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string): Promise<DeleteResult> {
+    return this.usersService.remove(id);
+  }
+}
