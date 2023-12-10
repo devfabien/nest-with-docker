c715bffbe91a41456ce77c469f11084360c189c6
devfabien <ish001fabi@gmail.com>
2023-12-10 13:20:04 +0200

    refactor: updating the expiration time for the token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { RolesGuard } from 'src/users/roles.guard';
     JwtModule.register({
       global: true,
       secret: jwtConstraints.secret,
-      signOptions: { expiresIn: '1h' },
+      signOptions: { expiresIn: '1d' },
     }),
   ],
   providers: [
