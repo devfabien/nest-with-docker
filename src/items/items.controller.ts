0dd7f0d55475ad8d03f066d3161b0921dccce560
devfabien <ish001fabi@gmail.com>
2023-12-11 10:53:06 +0200

    feat: creating all the endpoints for the items service

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+} from '@nestjs/common';
+import { ItemsService } from './items.service';
+import { Item } from './schemas/item.entity';
+import { UpdateResult, DeleteResult } from 'typeorm';
+import { CreateItemDto } from './schemas/dto/create-item.dto';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+  constructor(private itemService: ItemsService) {}
+
+  @Get()
+  async getAllUsers() {
+    return this.itemService.findAll();
+  }
+
+  @Get(':id')
+  async getUser(@Param('id') id: string): Promise<Item> {
+    return this.itemService.findOne(id);
+  }
+
+  @Post()
+  async createUser(
+    @Body() item: CreateItemDto,
+    @Req() req: any,
+  ): Promise<Item> {
+    const user = req.user;
+    return this.itemService.create(user, item);
+  }
+
+  @Patch(':id')
+  async updateUSer(
+    @Param('id') id: string,
+    @Body() item: Item,
+  ): Promise<UpdateResult> {
+    return this.itemService.update(id, item);
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string): Promise<DeleteResult> {
+    return this.itemService.remove(id);
+  }
+}
